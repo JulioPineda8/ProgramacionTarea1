12410aa607a8265c5bfc96dac483ab7eeed28c05
JulioPineda <jpinedad5@miumg.edu.gt>
2024-02-13 18:34:18 -0600

    primer commit

---
+++ multiplos.py
@@ -0,0 +1,12 @@
+#multiplicar
+"""
+Si quieres varias copias de una cadena de caracteres utiliza el 
+operador de multiplicación (*). En este ejemplo, la cadena de 
+caracteres mensaje2a lleva el contenido “Hola” tres veces, 
+mientras que la cadena de caracteres mensaje2b tiene el contenido 
+“Mundo”. Ordenemos imprimir las dos cadenas
+"""
+
+mensaje2a = 'Hola ' * 3
+mensaje2b = 'Mundo'
+print(mensaje2a + mensaje2b)
